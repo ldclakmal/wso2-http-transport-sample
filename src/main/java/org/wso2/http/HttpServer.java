fce874801297b2c431354729eb58ec3300fa87a4
ldclakmal <ldclakmal@gmail.com>
2018-10-16 11:31:17 +0530

    Add http server sample

---
+++ src/main/java/org/wso2/http/HttpServer.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.http;
+
+import org.wso2.transport.http.netty.contract.HttpWsConnectorFactory;
+import org.wso2.transport.http.netty.contract.ServerConnector;
+import org.wso2.transport.http.netty.contract.ServerConnectorFuture;
+import org.wso2.transport.http.netty.contract.config.ListenerConfiguration;
+import org.wso2.transport.http.netty.contract.config.ServerBootstrapConfiguration;
+import org.wso2.transport.http.netty.contractimpl.DefaultHttpWsConnectorFactory;
+
+import java.util.HashMap;
+
+import static org.wso2.transport.http.netty.contract.Constants.HTTP_SCHEME;
+
+/**
+ * An HTTP server which implemented using wso2 http-transport.
+ */
+public class HttpServer {
+
+    private static final int SERVER_PORT = 9090;
+
+    public static void main(String[] args) throws InterruptedException {
+        HttpWsConnectorFactory factory = new DefaultHttpWsConnectorFactory();
+        ListenerConfiguration listenerConfiguration = getListenerConfiguration();
+        ServerConnector connector = factory
+                .createServerConnector(new ServerBootstrapConfiguration(new HashMap<>()), listenerConfiguration);
+        ServerConnectorFuture future = connector.start();
+        future.setHttpConnectorListener(new EchoMessageListener());
+        future.sync();
+    }
+
+    private static ListenerConfiguration getListenerConfiguration() {
+        ListenerConfiguration listenerConfiguration = ListenerConfiguration.getDefault();
+        listenerConfiguration.setPort(SERVER_PORT);
+        listenerConfiguration.setScheme(HTTP_SCHEME);
+        return listenerConfiguration;
+    }
+}
